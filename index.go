e59bb8732bc311054ede56296498039134ac8f2f
Rasmus Jönsson <rasmus@rtek.se>
2016-07-15 19:52:54 +0200

    Go vet correct format and gofmt runned

--- index.go
+++ index.go
@@ -5,15 +5,15 @@ import (
 	"net/http"
 )
 
-func index(ctx *Context){
-    if ctx.Settings.TwilioId == "" {
-        settingsError(ctx.Response)
-        return 
-    }
-    tmpl, err := template.ParseFiles("global/main.html","global/footer.html","global/header.html","global/scripts.html","global/css.html", "site/startpage.html")
-    if err != nil {
-        http.Error(ctx.Response, "This website is currently unavailable :(", 502)
-        return   
-    }
-    tmpl.Execute(ctx.Response, buildPageModel())
+func index(ctx *Context) {
+	if ctx.Settings.TwilioId == "" {
+		settingsError(ctx.Response)
+		return
+	}
+	tmpl, err := template.ParseFiles("global/main.html", "global/footer.html", "global/header.html", "global/scripts.html", "global/css.html", "site/startpage.html")
+	if err != nil {
+		http.Error(ctx.Response, "This website is currently unavailable :(", 502)
+		return
+	}
+	tmpl.Execute(ctx.Response, buildPageModel())
 }
--- main.go
+++ main.go
@@ -1,67 +1,66 @@
 package main
 
 import (
-    "net/http"
-    "io/ioutil"
-    "encoding/json"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"io/ioutil"
+	"net/http"
 )
 
 func buildPageModel() interface{} {
-    return struct{
-        Name string
-        Email string
-        Age int
-    }{
-        "Rasmus Jönsson",
-        "hello@rasmusj.se",
-        21,
-    }
+	return struct {
+		Name  string
+		Email string
+		Age   int
+	}{
+		Name: "Rasmus Jönsson",
+		Email: "hello@rasmusj.se",
+		Age: 21,
+	}
 }
 
-type Settings struct{
-    TwilioId    string
-    TwilioKey   string
+type Settings struct {
+	TwilioId  string
+	TwilioKey string
 }
 
-type Context struct{
-    AE          context.Context
-    Settings    *Settings
-    Response    http.ResponseWriter
-    Request     *http.Request
-    Vars        map[string]string
+type Context struct {
+	AE       context.Context
+	Settings *Settings
+	Response http.ResponseWriter
+	Request  *http.Request
+	Vars     map[string]string
 }
 
-func init()  {
-    
-    var settings Settings
-    file, err := ioutil.ReadFile("./settings_prod.json")
-    if err == nil {
-        json.Unmarshal(file, &settings)
-    }
-    
-    r := mux.NewRouter()
-    r.HandleFunc("/", RequestWrapper(index, &settings))
-    r.HandleFunc("/{page}", RequestWrapper(page, &settings))
-    http.Handle("/", r)
+func init() {
+
+	var settings Settings
+	file, err := ioutil.ReadFile("./settings_prod.json")
+	if err == nil {
+		json.Unmarshal(file, &settings)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", RequestWrapper(index, &settings))
+	r.HandleFunc("/{page}", RequestWrapper(page, &settings))
+	http.Handle("/", r)
 }
 
-func settingsError(w http.ResponseWriter){
-    http.Error(w, "Could not read site configuration file!", 502)
+func settingsError(w http.ResponseWriter) {
+	http.Error(w, "Could not read site configuration file!", 502)
 }
 
-func RequestWrapper(endpoint func(ctx *Context), settings *Settings) (func(w http.ResponseWriter, r *http.Request)){
-    return func(w http.ResponseWriter, r *http.Request){
-        c := appengine.NewContext(r)
-        endpoint(&Context{
-            AE: c,
-            Settings: settings,
-            Request: r,
-            Response: w,
-            Vars: mux.Vars(r),
-        })
-    }
+func RequestWrapper(endpoint func(ctx *Context), settings *Settings) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := appengine.NewContext(r)
+		endpoint(&Context{
+			AE:       c,
+			Settings: settings,
+			Request:  r,
+			Response: w,
+			Vars:     mux.Vars(r),
+		})
+	}
 }
-
